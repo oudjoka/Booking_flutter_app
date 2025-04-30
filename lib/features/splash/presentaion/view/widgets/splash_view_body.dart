12a2ec5a61804eea70047bef7fa230a0ac5f4905
Koumni Ahmed <koumniahmed05@gmail.com>
2025-04-30 10:05:09 +0100

    adding sliding text animation in splash view

--- lib/features/splash/presentaion/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentaion/view/widgets/splash_view_body.dart
@@ -1,8 +1,39 @@
 import 'package:booking/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +43,15 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.logo),
         SizedBox(height: 4),
-        Text('Get Free Book', textAlign: TextAlign.center),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: Text('Get Free Book', textAlign: TextAlign.center),
+            );
+          },
+        ),
       ],
     );
   }
