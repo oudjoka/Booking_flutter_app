450ec1e09ebb32d19704f3ed9d7e94b6490ca1cc
Koumni Ahmed <koumniahmed05@gmail.com>
2025-05-01 17:11:03 +0100

    add featured book list view to home

---
+++ lib/features/home/presenataion/view/widgets/featured_books_list_view.dart
@@ -0,0 +1,19 @@
+import 'package:booking/features/home/presenataion/view/widgets/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return FeaturedListViewItem();
+        },
+      ),
+    );
+  }
+}
--- lib/features/home/presenataion/view/widgets/home_view_body.dart
+++ lib/features/home/presenataion/view/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:booking/core/utils/assets.dart';
 import 'package:booking/features/home/presenataion/view/widgets/custom_app_bar.dart';
 import 'package:booking/features/home/presenataion/view/widgets/custom_list_view_item.dart';
+import 'package:booking/features/home/presenataion/view/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,6 +9,6 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar(), CustomListViewItem()]);
+    return Column(children: [CustomAppBar(), FeaturedBooksListView()]);
   }
 }
