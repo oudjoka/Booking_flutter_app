4d1ded90f8c239ffad9e36d646fcf841e5ade4a6
Koumni Ahmed <koumniahmed05@gmail.com>
2025-05-03 01:10:29 +0100

    create app styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,27 @@
+import 'package:booking/constant.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMeduim = TextStyle(
+  static const textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSoectraFine,
+  );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSoectraFine,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle16 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
   );
 }
--- lib/features/home/presenataion/view/widgets/home_view_body.dart
+++ lib/features/home/presenataion/view/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:booking/core/utils/assets.dart';
 import 'package:booking/core/utils/styles.dart';
 import 'package:booking/features/home/presenataion/view/widgets/custom_app_bar.dart';
 import 'package:booking/features/home/presenataion/view/widgets/featured_books_list_view.dart';
@@ -16,8 +17,33 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           SizedBox(height: 50),
-          Text('Best Seller', style: Styles.titleMeduim),
+          Text('Best Seller', style: Styles.textStyle18),
+          BestSellerListViewItem(),
         ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.red,
+            image: DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
       ),
     );
   }
